5cb3d3b8fd3ea2e2a4fb6c9b06a2cf13b941875d
harits1702 <95862271+harits1702@users.noreply.github.com>
2023-10-23 07:33:45 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,13 @@
-
+<?php
+require 'conn.php';
+function viewData($query)
+{
+    global $koneksi;
+    $result = mysqli_query($koneksi, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+?>
